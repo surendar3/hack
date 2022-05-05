38c0e2a3dad3edbe8592f33ddea1168b65d08f12
surendar3 <102233525+surendar3@users.noreply.github.com>
2022-05-05 17:02:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const pokeApi = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=0";
+
+const apiResponse = async (api=pokeApi) => {
+  let response = await fetch(api);
+  return await response.json();
+};
+
+const createTable = async () => {
+  const pokes = await apiResponse();
+  let tbodyHtml = ``;
+  let c=0;
+  await pokes.results.map(async (pokemon, i) => {
+    const pokemonData = await apiResponse(pokemon.url);
+    const abilities = pokemonData.abilities.map(ele=> ele.ability.name)
+    const moves = pokemonData.moves.map(ele=> ele.move.name);
+    document.querySelector("#tbody").innerHTML+=`<tr>
+    <td>${i+1}</td>
+    <td>${pokemon.name}</td>
+    <td>${pokemonData.weight}</td>
+    <td>${abilities}</td>
+    <td>${moves}</td>
+    </tr>`
+  });
+  document.getElementById("pokemonTable").hidden=false;
+  document.getElementById("loader").hidden=true;
+};
+
+createTable();
